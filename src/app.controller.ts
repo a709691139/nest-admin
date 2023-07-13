bd968d079e329a7033e3acb9eb60ca1b07b6be8e
peng <709691139@qq.com>
2023-07-13 17:16:04 +0800

    feat: 添加/1 /2测试

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiExtraModels, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Pagination } from './dto/Pagination';
+import { HttpCommonDataProvider } from './provider/HttpCommonDataProvider';
 
 @ApiTags('app')
 @ApiExtraModels(Pagination)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Inject()
+  private readonly httpCommonDataProvider: HttpCommonDataProvider;
 
+  count = 0;
   @ApiOperation({ summary: '简介', description: '说明' })
   @Get('/')
-  getHello(): string {
+  async getHello() {
+    this.httpCommonDataProvider.incrementCounter();
+    console.log(
+      'getHelloincrementCounter',
+      this.httpCommonDataProvider.getCounter(),
+    );
+    this.count++;
+    // console.log(`index1 / 当前进程的PID为：${process.pid}`, this.count);
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    // console.log('index1 count', this.count);
+    return this.appService.getHello();
+  }
+
+  @ApiOperation({ summary: '简介', description: '说明' })
+  @Get('/2')
+  async getHello2() {
+    this.count++;
+    console.log(`index2 / 当前进程的PID为：${process.pid}`, this.count);
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log('index2 count', this.count);
     return this.appService.getHello();
   }
 }
