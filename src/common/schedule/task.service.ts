a861f54732e50e1c413e2e466fb59c70be403bd9
peng <709691139@qq.com>
2024-04-03 17:49:21 +0800

    fix: 替换nestjs过期代码

--- src/common/schedule/task.service.ts
+++ src/common/schedule/task.service.ts
@@ -2,8 +2,8 @@ import { Role } from '@/modules/system/entity/role.entity';
 import { Global, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Cron, SchedulerRegistry } from '@nestjs/schedule';
-import { InjectConnection } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { GlobalDataService } from '../provider/GlobalDataService';
 
 @Global()
@@ -11,12 +11,10 @@ import { GlobalDataService } from '../provider/GlobalDataService';
 export class TaskService {
   constructor(
     private schedulerRegistry: SchedulerRegistry,
-    @InjectConnection() private connection: Connection,
+    @InjectDataSource() private dataSource: DataSource,
     private readonly configService: ConfigService,
     private readonly globalDataService: GlobalDataService,
-  ) {
-    Logger.debug('TaskService constructor');
-  }
+  ) {}
 
   private readonly logger = new Logger(TaskService.name);
 
@@ -24,7 +22,7 @@ export class TaskService {
   async queryRoles() {
     this.logger.debug('queryRoles ' + '执行');
     // 查询全部角色并存入本地变量
-    const entityManager = this.connection.manager;
+    const entityManager = this.dataSource.manager;
     const roles = await entityManager.find(Role, {
       select: ['id', 'name', 'tenantId'],
       relations: ['permissions'],
